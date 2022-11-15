218eac9fa8e6ba620115d3e00991daf0a754355b
SeobinYun <o0oseobin@naver.com>
2022-11-15 19:35:08 +0900

    Seobin Branch에 업데이트

--- app/src/main/java/com/example/udoncar/myPageActivity.java
+++ app/src/main/java/com/example/udoncar/myPageActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Screen_Login extends AppCompatActivity {
+public class myPageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_screen_login);
+        setContentView(R.layout.activity_mypage);
     }
 }
