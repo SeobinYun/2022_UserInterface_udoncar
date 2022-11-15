68b69bdd4ce6cf64a6d47fad7c9054bc9fc3d642
백성아 <gwrw1518@naver.com>
2022-11-15 21:33:00 +0900

    AndroidManifest.xml/채팅클래스 이름변환

--- app/src/main/java/com/example/udoncar/ChattingActivity.java
+++ app/src/main/java/com/example/udoncar/ChattingActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Screen_Chatting extends AppCompatActivity {
+public class ChattingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_screen_chatting);
+        setContentView(R.layout.activity_chatting);
     }
 }
