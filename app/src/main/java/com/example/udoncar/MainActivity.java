7fd0b024c754c13cb1b74de43c7fbae518c8cd5c
SeobinYun <o0oseobin@naver.com>
2022-11-16 13:31:33 +0900

    폰트, <조건>, MainActivity.java 수정

--- app/src/main/java/com/example/udoncar/MainActivity.java
+++ app/src/main/java/com/example/udoncar/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_screen_login);
+        setContentView(R.layout.activity_login);
     }
 }
