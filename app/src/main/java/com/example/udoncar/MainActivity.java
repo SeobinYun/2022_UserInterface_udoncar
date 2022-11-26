ea770a82d8822357cbc99884a7500edba69521fe
백성아 <gwrw1518@naver.com>
2022-11-27 02:59:46 +0900

    1

--- app/src/main/java/com/example/udoncar/MainActivity.java
+++ app/src/main/java/com/example/udoncar/MainActivity.java
@@ -2,36 +2,59 @@ package com.example.udoncar;
 
 import static android.content.ContentValues.TAG;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 
-import android.net.Uri;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
 
+import com.example.udoncar.model.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity<mDatabase> extends AppCompatActivity {
     private FirebaseAuth mAuth;
 
     private ArrayList<MainData> arrayList;
     private MainAdapter mainAdapter;
     private RecyclerView recyclerView;
     private LinearLayoutManager linearLayoutManager;
-    //mainactiviy intent 삭제
+
+//    private FirebaseFirestore db;
+//    private String loca;
+//    private User curruntUser;
+//
+//    private TextView locaTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        db = FirebaseFirestore.getInstance();
+//        DocumentReference myRef = db.collection("users").document(loca);
+//        myRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+//            @Override
+//            public void onSuccess(DocumentSnapshot documentSnapshot) {
+//                curruntUser = documentSnapshot.toObject(User.class);
+//            }
+//        });
+//
+//        locaTextView = findViewById(R.id.location);
 
         recyclerView = (RecyclerView)findViewById(R.id.main_rv);
         linearLayoutManager = new LinearLayoutManager(this);
@@ -41,12 +64,8 @@ public class MainActivity extends AppCompatActivity {
 
         mainAdapter = new MainAdapter(arrayList);
         recyclerView.setAdapter(mainAdapter);
-
     }
-//
-//    public MainActivity(Intent intent) {
-//
-//    }
+
 
 
     // 사용자 정보 액세스
--- app/src/main/java/com/example/udoncar/MainDialogActivity.java
+++ app/src/main/java/com/example/udoncar/MainDialogActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,18 +13,31 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainDialogActivity extends AppCompatActivity implements View.OnClickListener {
     Button filterBtn;
 
-    AlertDialog dialog;
-    private DialogInterface.OnClickListener dialogListener;
+    AlertDialog myDialog;
 
     @Override
     protected void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
-        setContentView(R.layout.activity_main_dialog);
+        setContentView(R.layout.activity_main);
 
         filterBtn = findViewById(R.id.filter_btn);
 
         filterBtn.setOnClickListener(this);
     }
+
+    private void showToast(String message){
+        Toast toast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    DialogInterface.OnClickListener dialogListener = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            if (dialog == myDialog && which == DialogInterface.BUTTON_POSITIVE) {
+                showToast("myDialog 확인 click...");
+            }
+        }
+    };
 
     @Override
     public void onClick(View view) {
@@ -34,8 +48,8 @@ public class MainDialogActivity extends AppCompatActivity implements View.OnClic
             builder.setView(dialogView);
             builder.setPositiveButton("SUBMIT", dialogListener);
             builder.setNegativeButton("CANCEL", null);
-            dialog = builder.create();
-            dialog.show();
+            myDialog = builder.create();
+            myDialog.show();
         }
     }
 }
