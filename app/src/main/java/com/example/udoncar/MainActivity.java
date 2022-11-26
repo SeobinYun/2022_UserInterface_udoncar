573d6261978e8048a9dca2df2cdec298fa0f886e
SeobinYun <o0oseobin@naver.com>
2022-11-27 02:31:21 +0900

    하단바 - 프래그먼트 수정

--- app/src/main/java/com/example/udoncar/MainActivity.java
+++ app/src/main/java/com/example/udoncar/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.udoncar;
 
 import static android.content.ContentValues.TAG;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 
@@ -9,34 +10,71 @@ import android.net.Uri;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    HomeFragment homeFragment;
+    WriteFragment writeFragment;
+    HistoryFragment historyFragment;
+    MypageFragment mypageFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        homeFragment = new HomeFragment();
+        writeFragment = new WriteFragment();
+        historyFragment = new HistoryFragment();
+        mypageFragment = new MypageFragment();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.containers, homeFragment).commit();
+
+        NavigationBarView navigationBarView = findViewById(R.id.bottom_menu);
+        navigationBarView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch(item.getItemId()){
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.containers, homeFragment).commit();
+                        return true;
+                    case R.id.write:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.containers, writeFragment).commit();
+                        return true;
+                    case R.id.history:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.containers, historyFragment).commit();
+                        return true;
+                    case R.id.mypage:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.containers, mypageFragment).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
     }
-//
-//    public MainActivity(Intent intent) {
-//
-//    }
 
-
-    // 사용자 정보 액세스
-    private void getCurrentUser(){
+    public void checkCurrentUser(){
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user != null) {
-            // Name, email address, and profile photo Url
-            Log.d(TAG,"getCurrentUser 성공");
+        if(user != null){
+            // User is signed in
+        }
+        else{
+            // No user is signed in
         }
     }
+
+
 
     @Override
     public void onStart() {
@@ -49,6 +87,4 @@ public class MainActivity extends AppCompatActivity {
 //        }
     }
 
-    private void reload() {
-    }
 }
---
+++ app/src/main/java/com/example/udoncar/MypageFragment.java
@@ -0,0 +1,73 @@
+package com.example.udoncar;
+
+import static android.content.ContentValues.TAG;
+
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MypageFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_mypage, container, false);
+    }
+
+    public void getUserProfile() {
+        // [START get_user_profile]
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // Name, email address, and profile photo Url
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+            Uri photoUrl = user.getPhotoUrl();
+
+            // Check if user's email is verified
+            boolean emailVerified = user.isEmailVerified();
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getIdToken() instead.
+            String uid = user.getUid();
+        }
+        // [END get_user_profile]
+    }
+
+    // 회원탈퇴
+    public void deleteUser() {
+        // [START delete_user]
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        user.delete()
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "User account deleted.");
+                        }
+                    }
+                });
+        // [END delete_user]
+    }
+
+    // 로그아웃
+    public void signOut() {
+        // [START auth_sign_out]
+        FirebaseAuth.getInstance().signOut();
+        // [END auth_sign_out]
+    }
+
+}
